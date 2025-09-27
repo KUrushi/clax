24014c50a2d25ff090ed83f05258f154a100bb38
KUrushi <delver.rasping_08@icloud.com>
2025-09-27 14:56:00 +0900

    一次元で動くコード

--- cffi.lisp
+++ cffi.lisp
@@ -1,39 +1,54 @@
 (ql:quickload :cffi)
 
-;; 共有ライブラリの定義
+;; --- Foreign Library Definition ---
 (cffi:define-foreign-library libxla-wrapper
   (:darwin "/Users/urushiyama.k/quicklisp/local-projects/clax/xla/bazel-bin/xla/lisp_bridge/libxla_wrapper.so")
   (:unix "/Users/urushiyama.k/quicklisp/local-projects/clax/xla/bazel-bin/xla/lisp_bridge/libxla_wrapper.so")
   (:t (:default "libxla_wrapper")))
 
 (cffi:use-foreign-library libxla-wrapper)
-
-(cffi:defcfun ("hello_xla_pjrt" hello-xla-pjrt) :void)
-(cffi:defcfun ("initialize_client" initialize-client) :pointer)
-(cffi:defcfun ("get_device" get-device) :pointer
-  (client :pointer)
-  (device-index :size))
-(cffi:defcfun ("destroy_client" destroy-client) :void
-  (client :pointer))
-
-
-
-(defmacro with-pjrt-client ((client-var) &body body)
-  "PJRTクライアントを初期化し、BODYを実行した後、確実にクライアントを破棄するマクロ。"
-  `(let ((,client-var (initialize-client)))
-     (unwind-protect
-          (progn ,@body)
-       (format t "~&Cleaning up PJRT Client...~%")
-       (destroy-client ,client-var))))
-
+;; --- PJRT Data Types & Macros ---
 (cffi:defcenum pjrt-buffer-type
-  (:f32 10)
-  (:f64 11)
+  (:invalid 0)
+  (:pred 1)
+  (:s8 2)
+  (:s16 3)
   (:s32 4)
   (:s64 5)
-  (:i64 3))
+  (:u8 6)
+  (:u16 7)
+  (:u32 8)
+  (:u64 9)
+  (:f16 10)
+  (:f32 11)
+  (:f64 12)
+  (:bf16 13)
+  (:c64 14)
+  (:c128 15)
+  (:f8e5m2 16)
+  (:f8e4m3fn 17)
+  (:f8e4m3b11fnuz 18)
+  (:f8e5m2fnuz 19)
+  (:f8e4m3fnuz 20)
+  (:s4 21)
+  (:u4 22)
+  (:token 23)
+  (:s2 24)
+  (:u2 25)
+  (:f8e4m3 26)
+  (:f8e3m4 27)
+  (:f8e8m0fnu 28)
+  (:f4e2m1fn 29))
 
 
+;; --- C Function Definitions (FFI) ---
+(cffi:defcfun ("initialize_pjrt_api" initialize-pjrt-api) :void)
+(cffi:defcfun ("shutdown_pjrt_api" shutdown-pjrt-api) :void)
+(cffi:defcfun ("create_client" create-client) :pointer)
+(cffi:defcfun ("destroy_client" destroy-client) :void (client :pointer))
+(cffi:defcfun ("get_device" get-device) :pointer (client :pointer) (device-index :size))
+
+;; MODIFIED FUNCTION DEFINITION
 (cffi:defcfun ("create_buffer_from_host" create-buffer-from-host) :pointer
   (client :pointer)
   (device :pointer)
@@ -42,77 +57,92 @@
   (num-dims :size)
   (type pjrt-buffer-type))
 
+(cffi:defcfun ("buffer_to_host" buffer-to-host) :void
+  (buffer :pointer) (data-ptr :pointer) (byte-size :size))
+(cffi:defcfun ("compile_add_program" compile-add-program) :pointer
+  (client :pointer))
+(cffi:defcfun ("destroy_executable" destroy-executable) :void
+  (executable :pointer))
+(cffi:defcfun ("execute_add" execute-add) :pointer
+  (executable :pointer)
+  (buffer-a :pointer)
+  (buffer-b :pointer))
+
+
+(defmacro with-pjrt-api (() &body body)
+  `(progn
+     (initialize-pjrt-api)
+     (unwind-protect
+          (progn ,@body)
+       (shutdown-pjrt-api))))
+
+(defmacro with-pjrt-client ((client-var) &body body)
+  `(let ((,client-var (create-client)))
+     (unless (cffi:null-pointer-p ,client-var)
+       (unwind-protect
+            (progn ,@body)
+         (destroy-client ,client-var)))))
+
+;; --- Helper Function ---
 (defun tensor-from-host (client device vector)
-  "Lispの1次元配列 `vector` からPJRT_Bufferを作成する高レベル関数。"
   (let* ((dims (list (length vector)))
          (num-dims (length dims))
-         ;; 型判定部分を cond と equal を使った形に変更
          (array-type (array-element-type vector))
          (cffi-type (cond
                       ((equal array-type 'single-float) :float)
                       ((equal array-type 'double-float) :double)
-                      ((equal array-type '(signed-byte 32)) :int32)
-                      ((equal array-type '(signed-byte 64)) :int64)
                       (t (error "Unsupported array element type: ~A" array-type)))))
-
-    (cffi:with-foreign-object (dims-ptr :int64 num-dims)
-    (cffi:with-foreign-object (data-ptr cffi-type (length vector))
-
+    (cffi:with-foreign-objects ((dims-ptr :int64 num-dims)
+                                (data-ptr cffi-type (length vector)))
       (loop for i from 0 for dim in dims
             do (setf (cffi:mem-aref dims-ptr :int64 i) dim))
-
       (loop for i from 0 below (length vector)
             do (setf (cffi:mem-aref data-ptr cffi-type i) (aref vector i)))
-
       (let ((pjrt-type (ecase cffi-type
                          (:float :f32)
-                         (:double :f64)
-                         (:int32 :s32)
-                         (:int64 :s64))))
-        (create-buffer-from-host client device data-ptr dims-ptr num-dims pjrt-type))))))
+                         (:double :f64))))
+        ;; This call works correctly because CFFI translates the :f32 keyword
+        ;; to its integer value (10) before passing it to the C function.
+        (create-buffer-from-host client device data-ptr dims-ptr num-dims pjrt-type)))))
 
-(cffi:defcfun ("execute_add" execute-add) :pointer
-  (client :pointer)
-  (buffer-a :pointer)
-  (buffer-b :pointer))
-
-(cffi:defcfun ("buffer_to_host" buffer-to-host) :void
-  (buffer :pointer)
-  (data-ptr :pointer)
-  (byte-size :size))
-
-;; 高レベルなテスト実行
+;; --- Main Test Execution ---
 (defun test-run ()
-(with-pjrt-client (client)
-  (let ((device (get-device client 0)))
-    (format t "--- Preparing Tensors ---~%")
-    ;; 1. 入力となる2つのLisp配列を作成
-    (let* ((vec-a (make-array 2 :element-type 'single-float :initial-contents '(10.0 20.0)))
-           (vec-b (make-array 2 :element-type 'single-float :initial-contents '(1.5 2.5)))
-
-           ;; 2. Lisp配列からデバイス上のバッファを作成
-           (buf-a (tensor-from-host client device vec-a))
-           (buf-b (tensor-from-host client device vec-b)))
-
-      (format t "Input Buffer A: ~A~%" buf-a)
-      (format t "Input Buffer B: ~A~%" buf-b)
-
-      (format t "~%--- Executing Add Operation ---~%")
-      ;; 3. 足し算を実行し、結果のバッファを取得
-      (let ((result-buf (execute-add client buf-a buf-b)))
-        (format t "Result Buffer: ~A~%" result-buf)
-
-        (format t "~%--- Copying Result to Lisp ---~%")
-        ;; 4. 結果をLispにコピーしてくる
-        ;;    結果を格納するためのCのメモリ領域を確保
-        (cffi:with-foreign-object (result-ptr :float 2)
-          ;; Cの関数を呼び出してデータをコピー
-          (buffer-to-host result-buf result-ptr (* 2 (cffi:foreign-type-size :float)))
-
-          ;; CのメモリからLispのベクターに値を変換
-          (let ((result-vec (make-array 2 :element-type 'single-float)))
-            (loop for i from 0 below 2
-                  do (setf (aref result-vec i) (cffi:mem-aref result-ptr :float i)))
-
-            ;; 5. 最終結果を表示！
-            (format t "Result values: ~A~%" result-vec))))))))
+  (with-pjrt-api ()
+    (with-pjrt-client (client)
+      (format t "~&--- Compiling the 'add' function once ---~%")
+      (let ((add-executable (compile-add-program client)))
+        ;; This 'unwind-protect' guarantees the executable is destroyed,
+        ;; but only if it was successfully created.
+        (unwind-protect
+             (progn
+               ;; First, check if compilation succeeded before trying to use the executable
+               (if (cffi:null-pointer-p add-executable)
+                   (format t "~&Error: Failed to compile the program.~%")
+                   ;; This is the "ELSE" block: Compilation was successful
+                   (progn
+                     (format t "Compilation successful. Executable: ~A~%" add-executable)
+                     (let ((device (get-device client 0)))
+                       (format t "~&--- Preparing Tensors ---~%")
+                       (let* ((vec-a (make-array 2 :element-type 'single-float :initial-contents '(10.0 20.0)))
+                              (vec-b (make-array 2 :element-type 'single-float :initial-contents '(1.5 2.5)))
+                              (buf-a (tensor-from-host client device vec-a))
+                              (buf-b (tensor-from-host client device vec-b)))
+                         (format t "Input Buffer A: ~A~%" buf-a)
+                         (format t "Input Buffer B: ~A~%" buf-b)
+                         (format t "~%--- Executing Add Operation ---~%")
+                         (let ((result-buf (execute-add add-executable buf-a buf-b)))
+                           (format t "Result Buffer: ~A~%" result-buf)
+                           (if (cffi:null-pointer-p result-buf)
+                               (format t "~&Execution failed.~%")
+                               (progn
+                                 (format t "~%--- Copying Result to Lisp ---~%")
+                                 (cffi:with-foreign-object (result-ptr :float 2)
+                                   (buffer-to-host result-buf result-ptr (* 2 (cffi:foreign-type-size :float)))
+                                   (let ((result-vec (make-array 2 :element-type 'single-float)))
+                                     (loop for i from 0 below 2
+                                           do (setf (aref result-vec i) (cffi:mem-aref result-ptr :float i)))
+                                     (format t "Result values: ~A~%" result-vec)))))))))))
+          ;; Cleanup form for the unwind-protect
+          (when add-executable
+            (format t "~&Cleaning up compiled executable...~%")
+            (destroy-executable add-executable)))))))
